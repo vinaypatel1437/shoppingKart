bfd29fb5b57d6fdfae414f023e1dc7e5e5db0d9d
Vinay Patelm <patel.vp.vinay@gmail.com>
2022-01-05 17:32:59 +0530

    Vinmay

---
+++ build/precache-manifest.3d21bbb87604eaabe373cc5c29815070.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6eebbcb5db1a4ee01adec3e9a3f9d93",
+    "url": "/index.html"
+  },
+  {
+    "revision": "223fc140facbc20c7008",
+    "url": "/static/css/main.ab5ea66f.chunk.css"
+  },
+  {
+    "revision": "a545394c8c3a953d1194",
+    "url": "/static/js/2.ac113c3a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.ac113c3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "223fc140facbc20c7008",
+    "url": "/static/js/main.8474fa49.chunk.js"
+  },
+  {
+    "revision": "b10d08823a4df677ba67",
+    "url": "/static/js/runtime-main.de931e19.js"
+  },
+  {
+    "revision": "0eb2e23869b3832645fdfca4925e8e06",
+    "url": "/static/media/bike.0eb2e238.jpg"
+  },
+  {
+    "revision": "028978f40ca009a385c6fb19486bdc53",
+    "url": "/static/media/book.028978f4.jpg"
+  },
+  {
+    "revision": "d7a3cb1c24a25ed54c98443744bed9cb",
+    "url": "/static/media/bottle.d7a3cb1c.jpg"
+  },
+  {
+    "revision": "479fe603534816b6342674f0b7f5999e",
+    "url": "/static/media/ipad.479fe603.jpeg"
+  },
+  {
+    "revision": "48183761474b5acd6ea4c2ef6843652b",
+    "url": "/static/media/laptop.48183761.jpeg"
+  },
+  {
+    "revision": "450a2ab1154f7b2c28e5c814b3e51b2c",
+    "url": "/static/media/mask.450a2ab1.png"
+  },
+  {
+    "revision": "064de1cd607fdd4e7c03334d277189e4",
+    "url": "/static/media/notebook.064de1cd.jpg"
+  },
+  {
+    "revision": "8b7216c60a886036b7434ca11763032b",
+    "url": "/static/media/pen.8b7216c6.jpg"
+  },
+  {
+    "revision": "67a487675e594f3b274f76d190aa011f",
+    "url": "/static/media/smartphone.67a48767.jpg"
+  },
+  {
+    "revision": "4a0dbe85527b88bfcea004afbf6c9fb8",
+    "url": "/static/media/speaker.4a0dbe85.jpg"
+  }
+]);
